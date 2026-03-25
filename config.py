dced9ac1bd200c0c01decf101cb224ad3b4cf529
tassnimelleuch <tasnim.eleuch@enis.tn>
2026-03-25 15:40:33 +0100

    first commit

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret')
+
+    JENKINS_URL      = os.getenv('JENKINS_URL')
+    JENKINS_USERNAME = os.getenv('JENKINS_USERNAME')
+    JENKINS_TOKEN    = os.getenv('JENKINS_TOKEN')
+    JENKINS_JOB      = os.getenv('JENKINS_JOB')
